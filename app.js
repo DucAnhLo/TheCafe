d6e9f31494c38c914c31fa2e8bc90d2eca9d5e25
Liamml <42917027+Liamml@users.noreply.github.com>
2021-12-07 04:22:33 +0000

    Added New File

---
+++ app.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(function(tab){
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(function(tabContent) {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(function(tab) {
+            tab.classList.remove('active')
+        })
+
+        tab.classList.add('active')
+
+        target.classList.add('active')
+    })
+})
